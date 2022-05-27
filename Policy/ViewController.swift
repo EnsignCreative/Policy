8b64ed33583e3ad7419322e8133a57ac13e00c06
Brad Ensign <51426961+EnsignCreative@users.noreply.github.com>
2022-05-26 23:47:24 -0400

    Initial Commit

---
+++ Policy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Policy
+//
+//  Created by Bradley Ensign on 5/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
